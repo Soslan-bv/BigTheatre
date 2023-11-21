e6f8f89f18bb7ea94b8ed63c2c961c35f11811fa
Byazrov_Soslan <soslan-bv@mail.ru>
2023-11-21 21:20:01 +0300

    create bigTheatre

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.productions__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        autoplay: false,
+        arrows: false,
+        infinite: true,
+        autoplaySpeed: 2000,
+        speed: 1000
+    });
+});
